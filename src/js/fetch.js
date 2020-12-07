02a209607ac6814590c355d2b6be7f9e9593e01d
Stíofán ♔ Ó Conchubhair <stevo.joc@gmail.com>
2020-12-07 20:06:11 +0000

    Create fetch.js

---
+++ src/js/fetch.js
@@ -0,0 +1,22 @@
+async function fetchText() {
+    let response = await fetch('./readme.txt');
+    let data = await response.text();
+    console.log('fetchText: ' + data)
+}
+
+fetchText();
+
+async function fetchText2() {
+    let response = await fetch('./non-existence.txt');
+
+    console.log(response.status); // 404
+    console.log(response.statusText); // Not Found
+
+    if (response.status === 200) {
+        let data = response.text();
+        // handle data
+        console.log('fetchText2: ' + data)
+    }
+}
+
+fetchText2();
